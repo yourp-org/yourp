45b026ca79d7d83e660b5769b48730079a635330
givka <givka.dev@gmail.com>
2024-07-03 23:57:02 +0200

    Add videojs

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,173 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./App.css";
 
-let main: HTMLVideoElement | null = null;
+import jelly from "./jelly.mp4";
+import manifest from "./manifest.m3u8";
+
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+let main: HTMLElement | null = null;
+
+export const VideoJS = (props: any) => {
+  const videoRef = React.useRef(null);
+  const playerRef = React.useRef(null);
+  const { options, onReady } = props;
+
+  React.useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement("video-js");
+
+      videoElement.classList.add("vjs-big-play-centered");
+      (videoRef.current as any).appendChild(videoElement);
+
+      const player = ((playerRef.current as any) = videojs(
+        videoElement,
+        options,
+        () => {
+          videojs.log("player is ready");
+          onReady && onReady(player);
+        }
+      ));
+
+      // You could update an existing player in the `else` block here
+      // on prop change, for example:
+    } else {
+      const player = playerRef.current as any;
+
+      player.autoplay(options.autoplay);
+      player.src(options.sources);
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  React.useEffect(() => {
+    const player = playerRef.current as any;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player style={{ width: "80%" }}>
+      <div ref={videoRef} />
+    </div>
+  );
+};
+
+const handleClick = (ref: HTMLElement) => {
+  if (!main) return;
+
+  if (main === ref) return;
+
+  main.className = "other";
+  ref.className = "main";
+  main = ref;
+};
+
+const addVideo = (ref: HTMLElement | null, url: string, index: number) => {
+  if (!ref || ref.childElementCount !== 0) return;
+
+  const video = ref.appendChild(document.createElement("video"));
+  video.setAttribute("loop", "");
+  video.setAttribute("mute", "");
+  video.setAttribute("autoplay", "");
+  video.setAttribute("controls", "");
+
+  video.onclick = (e) => {
+    e.preventDefault();
+    handleClick(ref);
+  };
+
+  // video.onloadedmetadata = () => {
+  //   console.log(url, video.videoWidth, video.videoHeight);
+  // };
+
+  const source = video.appendChild(document.createElement("source"));
+  source.setAttribute("src", url);
+  source.setAttribute("type", url.endsWith("mp4") ? "video/mp4" : "video/mp2t");
+};
+
+// const url1 =
+//   "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_30MB.mp4";
+const url1 =
+  "https://ev.phncdn.com/videos/202406/27/454396371/480P_2000K_454396371.mp4?validfrom=1720038024&validto=1720045224&rate=500k&burst=1400k&ip=90.92.24.119&ipa=90.92.24.119&hash=6%2BnAUJEZxB%2BOdGiSkzDaCEp%2BjAs%3D";
+const url2 =
+  "https://ev.phncdn.com/videos/202406/07/453488691/480P_2000K_453488691.mp4?validfrom=1720038406&validto=1720045606&rate=500k&burst=1400k&ip=90.92.24.119&ipa=90.92.24.119&hash=65ne9M%2BmfORGuTo8TIG9RXzQR5s%3D";
+const url3 =
+  "https://test-videos.co.uk/vids/sintel/mp4/av1/360/Sintel_360_10s_10MB.mp4";
+const url4 =
+  "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4";
 
 const App = () => {
-  const [masterRef, setMasterRef] = useState<HTMLDivElement | null>(null);
+  const [mainRef, setMainRef] = useState<HTMLDivElement | null>(null);
   const [other1Ref, setOther1Ref] = useState<HTMLDivElement | null>(null);
   const [other2Ref, setOther2Ref] = useState<HTMLDivElement | null>(null);
   const [other3Ref, setOther3Ref] = useState<HTMLDivElement | null>(null);
 
-  const url1 =
-    "https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_30MB.mp4";
-  const url2 =
-    "https://test-videos.co.uk/vids/jellyfish/mp4/h264/360/Jellyfish_360_10s_30MB.mp4";
-  const url3 =
-    "https://test-videos.co.uk/vids/sintel/mp4/av1/360/Sintel_360_10s_10MB.mp4";
-  const url4 =
-    "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4";
-
-  const createStuff = (ref: HTMLElement | null, url: string, index: number) => {
-    if (!ref) return;
-    const video = ref.appendChild(document.createElement("video"));
-    video.setAttribute("autoplay", "");
-    video.setAttribute("loop", "");
-    video.setAttribute("mute", "");
-    video.setAttribute("controls", "");
-    const source = video.appendChild(document.createElement("source"));
-    source.setAttribute("src", url);
-    source.setAttribute("type", "video/mp4");
-    video.onclick = (e) => {
-      e.preventDefault();
-      handleClick(video);
-    };
-  };
-
-  const handleClick = (video: HTMLVideoElement) => {
-    if (main === video) return;
-    main = video;
-
-    const masterVideo = masterRef?.firstElementChild as HTMLVideoElement;
-
-    video.parentElement?.appendChild(masterVideo);
-    masterRef?.appendChild(video);
-
-    masterVideo.play();
-    masterVideo.muted = true;
-    video.play();
-    video.muted = true;
-  };
+  useEffect(() => {
+    if (!mainRef) return;
+    addVideo(mainRef, url1, 0);
+    mainRef.className = "main";
+    main = mainRef;
+  }, [mainRef]);
 
   useEffect(() => {
-    createStuff(masterRef, url1, 0);
-  }, [masterRef]);
-
-  useEffect(() => {
-    createStuff(other1Ref, url2, 1);
+    if (!other1Ref) return;
+    addVideo(other1Ref, url2, 1);
+    other1Ref.className = "other";
   }, [other1Ref]);
 
   useEffect(() => {
-    createStuff(other2Ref, url3, 1);
+    if (!other2Ref) return;
+    addVideo(other2Ref, url3, 1);
+    other2Ref.className = "other";
   }, [other2Ref]);
 
   useEffect(() => {
-    createStuff(other3Ref, url4, 1);
+    if (!other3Ref) return;
+    addVideo(other3Ref, url4, 1);
+    other3Ref.className = "other";
   }, [other3Ref]);
+
+  const videoJsOptions1 = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: url1,
+        type: "video/mp4",
+      },
+    ],
+  };
+
+  const videoJsOptions2 = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: url2,
+        type: "video/mp4",
+      },
+    ],
+  };
 
   return (
     <>
       <div className="App">
         <div className="the-grid">
-          <div className="main" ref={setMasterRef}></div>
-          <div className="others">
-            <div className="other1" ref={setOther1Ref}></div>
-            <div className="other2" ref={setOther2Ref}></div>
-            <div className="other3" ref={setOther3Ref}></div>
-          </div>
+          <VideoJS className="main" options={videoJsOptions1} />
+          <VideoJS className="other" options={videoJsOptions2} />
         </div>
       </div>
     </>
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="react-scripts" />
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.m3u8" {
+  const src: string;
+  export default src;
+}
